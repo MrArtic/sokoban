95099245daa1b49d083d6f37fd5f9062f8502511
Jens Kristian Espevik <jenskristian@gmail.com>
2013-03-31 17:33:34 +0200

    Minor config update and Slick2D game class

---
+++ src/sokoban1/SlickSokoban.java
@@ -0,0 +1,33 @@
+package sokoban1;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SlickSokoban extends BasicGame {
+
+	public SlickSokoban() {
+		super("Sokoban");
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
